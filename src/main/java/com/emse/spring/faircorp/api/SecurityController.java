bf990d609d3529fbed1f0e16b7dd77feef89f18e
loden09 <leo.hatstatt@etu.emse.fr>
2022-11-18 02:44:55 +0100

    very last commit

--- src/main/java/com/emse/spring/faircorp/api/SecurityController.java
+++ src/main/java/com/emse/spring/faircorp/api/SecurityController.java
@@ -2,10 +2,12 @@ package com.emse.spring.faircorp.api;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/api/security")
 public class SecurityController {
 
     @GetMapping(path = "/{id}")
